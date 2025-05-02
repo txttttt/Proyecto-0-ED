4ffdc7a0adae5e96f2c80c9ef8dfbd0ca995e042
txttttt <167579669+txttttt@users.noreply.github.com>
2025-05-02 04:26:16 +0000

    Update Header.h

--- Proyecto0 v,fail/Header.h
+++ Proyecto0 v,fail/Header.h
@@ -1,15 +1,15 @@
 /* Archivo creado por: Javier Lee Liang
 * Fecha creada: 27/04/2025
-* El archivo contiene el nombre de los m�todos que se van usar para 
+* El archivo contiene el nombre de los métodos que se van usar para 
 * compartir dentro del proyecto
-*/
+
 #pragma once
 
 #include <string>
 
 using std::string;
 
-// M�todos de "menu.cpp"
+// Métodos de "menu.cpp"
 void runningLoop(void (*f)(bool&, int&));
 void getMainMenuAction(bool& running, int& input);
 void getTicketMenuAction(bool& running, int& input);
@@ -18,7 +18,74 @@ void getServicesOptionsAction(bool& running, int& input);
 void getAreasOptionsAction(bool& running, int& input);
 void getUsersOptionsAction(bool& running, int& input);
 
-// M�todos de "userInput.cpp"
+// Métodos de "userInput.cpp"
 void getInt(const string text, int& input_int);
 void getStr(const string text, string& input_str);
+*/
+/* Archivo creado por: Javier Lee Liang
+* Fecha creada: 27/04/2025
+* El archivo contiene el nombre de los métodos que se van usar para 
+* compartir dentro del proyecto
+*/
+#pragma once
+
+#include <string>
+
+using std::string;
+
+// Métodos de "userInput.cpp"
+void getInt(const string text, int& input_int);
+void getStr(const string text, string& input_str);
+
+string const main_menu =
+"\nMenú principal\n"
+"---------------------------------\n"
+"0. Estado de las colas\n"
+"1. Agregar tiquetes\n"
+"2. Atender\n"
+"3. Administración\n"
+"4. Estadísticas del sistema\n"
+"5. Salir\n"
+"---------------------------------\n"
+"Seleccione alguna de las acciones: ";
+
+string const admin_menu =
+"\nMenú de administración\n"
+"---------------------------------\n"
+"0. Tipos de usuario\n"
+"1. Áreas\n"
+"2. Servicios disponibles\n"
+"3. Limpiar colas y estadísticas\n"
+"4. Regresar\n"
+"---------------------------------\n"
+"Seleccione alguna de las opciones a modificar: ";
+
+string const users_options =
+"\nOpciones de usuarios\n"
+"---------------------------------\n"
+"0. Agregar\n"
+"1. Eliminar\n"
+"2. Regresar\n"
+"---------------------------------\n"
+"Seleccione alguna de las opciones: ";
+
+string const areas_options =
+"\nOpciones de áreas\n"
+"---------------------------------\n"
+"0. Agregar\n"
+"1. Modificar cantdad de ventanillas\n"
+"2. Eliminar\n"
+"3. Regresar\n"
+"---------------------------------\n"
+"Seleccione alguna de las opciones: ";
+
+string const services_options =
+"\nOpciones de servicios\n"
+"---------------------------------\n"
+"0. Agregar\n"
+"1. Eliminar\n"
+"2. Reordernar\n"
+"3. Regresar\n"
+"---------------------------------\n"
+"Seleccione alguna de las opciones: ";
 
